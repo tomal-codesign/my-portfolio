0ebedb0d74ef32aefb844557eb3f87ebdfa4c854
Tomal Ahmed Raj <sj.tomalahmedraj@gmail.com>
2025-06-15 15:32:19 +0600

    first commit

---
+++ app/portfolio/page.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Portfolio = () => {
+    return (
+        <>
+            <div>Portfolio page</div>
+            <Link href="/">Home</Link>
+        </>
+    )
+}
+
+export default Portfolio
